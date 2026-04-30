f087ebaf43cca71c27b853589c760704d94d7cef
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-04-30 18:18:25 +0500

    js started

---
+++ lec01.js
@@ -0,0 +1,14 @@
+console.log("js lecture");
+
+const userName = "aiman mustafa";
+// userName ="areeba noor"
+console.log(userName);
+let userAge = 24;
+console.log(userAge);
+let marksObtain = 23.34;
+console.log(marksObtain);
+let isMarried = false;
+console.log(userName,"is married :" ,isMarried);
+
+
+
---
+++ lec02.js
@@ -0,0 +1,42 @@
+console.log("Operators");
+
+let a = 30;
+let b = 6;
+let sum = a+b;
+let differ = a%b;
+
+console.log("the modulus of a%b =", differ);
+
+let m = 54;
+m += 100;
+console.log(m);
+
+let numOne = 60;
+let numTwo = "60";
+console.log(numOne===numTwo);
+
+
+
+let userAge = 20;
+let userId = true;
+
+if(userAge!=20){
+    console.log("U CAN DRIVE");   
+}
+else{
+    console.log("U CAN'T DRIVE");
+}
+
+
+
+
+// case to write variables
+/**
+ * PascalCase
+ * camelCase ------
+ * snake_case
+ * kebab-case
+ */
+
+ 
+
